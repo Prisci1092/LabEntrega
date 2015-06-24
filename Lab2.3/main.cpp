550aba07f20e4bb6c19a694cbcb8208003aa512a
Prisci1092 <priarmu@gmail.com>
2015-06-23 22:37:20 -0600

    no message

---
+++ Lab2.3/main.cpp
@@ -0,0 +1,19 @@
+#include "Moneda.h"
+#include "Collar.h"
+#include "Corona.h"
+#include "Anillo.h"
+#include "Tesoro.h"
+#include "Pirata.h"
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    Tesoro t = Tesoro();
+    t.sTesoro();
+    return 0;
+
+}
+
